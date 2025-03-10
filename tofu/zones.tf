009c27bf2ca34ad377d2d93830e94312e2a94bd5
Ceci <ceci@sanare.dev>
2025-03-10 14:32:20 -0500

    fix: remove devkit.lgbt

--- tofu/zones.tf
+++ tofu/zones.tf
@@ -174,7 +174,6 @@ output "name_servers" {
   value = {
     "cecilias.me"       = module.cecilias_me.name_servers
     "rains.cafe"        = module.rains_cafe.name_servers
-    "devkit.lgbt"       = module.devkit_lgbt.name_servers
     "charcoal.gg"       = module.charcoal_gg.name_servers
     "sanare.dev"        = module.sanare_dev.name_servers
     "protontweaks.com"  = module.protontweaks_com.name_servers
